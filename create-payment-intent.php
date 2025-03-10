a6c3d9225b9eeb29834d7b223d4a9685324ca701
Hassan Fahes <hassanfahes67@gmail.com>
2025-03-10 23:54:56 +0200

    Update create-payment-intent.php

--- create-payment-intent.php
+++ create-payment-intent.php
@@ -1,7 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 
-\Stripe\Stripe::setApiKey('sk_test_51QJBGNK9JqRD1EchtmBO2ZRnfGcfK5RgGlNLwlR9ByElxLUeOIaioV5KzqYmFsWHE9DZMvfHWRX1MfgDUa37zzGc00QMl3Rqs6');
+\Stripe\Stripe::setApiKey('');
 
 try {
     // إعداد المبلغ وعملة الدفع (مثلاً: 10.00 دولار أمريكي = 1000 سنت)
